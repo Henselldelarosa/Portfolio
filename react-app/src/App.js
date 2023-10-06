953d5b83a03c09f28b823e872a5fbb9ae23d2f0f
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-10-06 01:21:24 -0400

    install useful tools

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import './App.scss'
 
 function App() {
   const dispatch = useDispatch();
@@ -14,19 +15,20 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
+    <div className="app">
+      hello
+      {/* <Navigation isLoaded={isLoaded} />
+      {isLoaded && ( */}
+        {/* <Switch>
           <Route path="/login" >
             <LoginFormPage />
           </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-        </Switch>
-      )}
-    </>
+        </Switch> */}
+       {/* )} */}
+    </div>
   );
 }
 
